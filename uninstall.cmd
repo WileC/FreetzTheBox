a93c960b5eb6a8a87cc248b6f72ed122d59bddcb
WileC <djwilec@d-jay.de>
2019-03-04 12:59:13 +0100

    Update uninstall.cmd

--- uninstall.cmd
+++ uninstall.cmd
@@ -1,11 +1,9 @@
 @echo off
 
-REM 	Skript:				uninstall.cmd
-REM 	Author:				WileC
-REM 	Web:				http://www.djwilec.de
-REM		Infos:				getestet mit Windows 10;
-REM							automatisierte Deinstallation der Skripte
-REM		Unterstützte Boxen:	3490, 4040, 6820
+REM     Skript:                 uninstall.cmd
+REM     Author:                 WileC
+REM     DL-Source:              https://github.com/WileC/FreetzTheBox
+REM     Web:                    http://www.djwilec.de
 
 REM PowerShell-Skripte zurücksetzen
 powershell -command {Set-ExecutionPolicy -ExecutionPolicy Default -Scope CurrentUser -Force}
